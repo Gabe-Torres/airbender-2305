2536f81283f28527dd4aa0afbcfc5d430a487242
Gabriel Torres <thomastgtorres21@gmail.com>
2023-09-08 11:18:17 -0500

    refactor into services/facades

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,19 +2,21 @@ class SearchController < ApplicationController
   def index
     
     nation = params[:nation]
-    conn = Faraday.new(url: 'https://last-airbender-api.fly.dev')
+    # conn = Faraday.new(url: 'https://last-airbender-api.fly.dev')
 
-    response = conn.get("/api/v1/characters?affiliation=#{nation}")
+    # response = conn.get("/api/v1/characters?affiliation=#{nation}")
 
-    data = JSON.parse(response.body, symbolize_names: true)
+    # data = JSON.parse(response.body, symbolize_names: true)
 
-    # @total_count = data.count
+    # # @total_count = data.count
 
-    member = data
+    # member = data
 
-    @members = member.map do |member_data|
-      Member.new(member_data)
-    end
+    # @members = member.map do |member_data|
+    #   Member.new(member_data)
+    # end
     # require 'pry'; binding.pry
+
+    @members = MembersFacade.members_by_nation(nation)
   end
 end
