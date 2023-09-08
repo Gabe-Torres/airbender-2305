605d593e963db93eab0b7804e5f01f06b6c33c8c
Gabriel Torres <thomastgtorres21@gmail.com>
2023-09-08 10:27:06 -0500

    connect to api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,13 @@
 class SearchController < ApplicationController
   def index
     
+    nation = params[:nation]
+    conn = Faraday.new(url: 'https://last-airbender-api.fly.dev')
+
+    response = conn.get("/api/v1/characters?affiliation=#{nation}")
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    require 'pry'; binding.pry
+    
   end
 end
